f534008f6c7b7384486c73cceb6386c460c23e6a
Kannan-Mtech <ramkannan434@gmail.com>
2025-04-08 23:04:44 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var box;
+
+function setup() {
+  createCanvas(800,800);
+  box = createSprite(800,800,50,50);
+
+}
+
+function draw() 
+{
+   background("cyan");
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    box.position.x = box.position.x +5;
+  }
+
+    if (keyIsDown(LEFT_ARROW)) 
+  {
+    box.position.x= box.position.x -5; 
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    box.position.y = box.position.y - 5;
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    box.position.y = box.position.y + 5;
+  }
+
+  drawSprites();
+}
+
+
+
+
